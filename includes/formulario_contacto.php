89e26e311708c766a0f53006fce1ad0e75e84a2a
hectorortizsuarez3 <hectorortizsuarez3@gmail.com>
2025-08-05 15:29:52 +0200

    formulario con estilos css adecuados

--- includes/formulario_contacto.php
+++ includes/formulario_contacto.php
@@ -1,5 +1,5 @@
 <form id="contactForm" action ="includes/procesar_formulario.php" method="post" novalidate>  <!--novalidate sirve para desactivar los mensajes por defecto del navegador-->
-    <div class=_"field">
+    <div class="field">
         <input id="nombre" type="text" name="nombre" placeholder="Nombre" required>
         <small class="error" id="error-nombre"></small>  <!--Creo contenedores para escribir el mensaje de error-->
     </div>
